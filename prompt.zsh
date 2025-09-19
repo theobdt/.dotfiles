1b38721619ad7c5b42836643ed5f9db878d1a58a
Théo Bodrito <theobodrito@meta.com>
2025-09-19 10:07:45 +0200

    Modifs

--- prompt.zsh
+++ prompt.zsh
@@ -3,6 +3,8 @@ ARROW="%(?:%{$fg_bold[green]%}➜  :%{$fg_bold[red]%}➜ "
 #PROMPT="%{$reset_color%}%{$fg[magenta]%}$(whoami)%{$fg_bold[magenta]%}@$(hostname)"
 PROMPT="%{$reset_color%}%{$fg_bold[magenta]%}@$(hostname)"
 PROMPT+=' %{$fg[cyan]%}%~%{$reset_color%} $(git_prompt_info)'
+#PROMPT+=' %{$fg[blue]%}%~%{$reset_color%} $(git_prompt_info)'
+#PROMPT+=' %{$fg[gray]%}%~%{$reset_color%} $(git_prompt_info)'
 PROMPT+=$'\n''$ARROW'
 
 ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg[red]%}("
